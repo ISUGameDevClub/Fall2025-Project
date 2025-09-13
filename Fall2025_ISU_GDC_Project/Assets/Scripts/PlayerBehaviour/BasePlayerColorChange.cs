e742bbb358b5628c2369ef4f6f18ffba0ca7a464
Jake Ozer <jakobozer1@gmail.com>
2025-09-12 21:20:12 -0500

    Set up local multiplayer

---
+++ Fall2025_ISU_GDC_Project/Assets/Scripts/PlayerBehaviour/BasePlayerColorChange.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class BasePlayerColorChange : MonoBehaviour
+{
+    //this script just assigns a random color to the sprite of a newly added player. 
+    //Will be removed once actual assets are in
+    private void Start()
+    {
+        if (GetComponent<SpriteRenderer>() != null)
+        {
+            GetComponent<SpriteRenderer>().color = Random.ColorHSV(0f, 1f, 0.8f, 1f,0.8f, 1f);
+        }
+    }
+}
--- Fall2025_ISU_GDC_Project/Assets/Scripts/PlayerBehaviour/PlayerMovement.cs
+++ Fall2025_ISU_GDC_Project/Assets/Scripts/PlayerBehaviour/PlayerMovement.cs
@@ -9,7 +9,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float groundedCheckLength;
     [SerializeField] private float coyoteTime;
     [SerializeField] private float jumpLock;
-    [SerializeField] private PlayerInput playerInput;
     [SerializeField] private LayerMask floorLayer;
     private Rigidbody2D rb;
     private Vector2 movement;
@@ -17,17 +16,25 @@ public class PlayerMovement : MonoBehaviour
     private float timer_jumpLock;
     private bool grounded;
     private bool jumpedThisFrame;
+    private bool queueJump;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        jumpedThisFrame = context.action.triggered;
+    }
+
     private void Update()
     {
-        //read movement input
-        movement = playerInput.actions["Move"].ReadValue<Vector2>();
-
         //ground check
         Debug.DrawRay(this.transform.position, new Vector2(0, -groundedCheckLength), Color.yellow);
         grounded = Physics2D.Raycast(this.transform.position, Vector2.down, groundedCheckLength, floorLayer);
@@ -44,11 +51,12 @@ public class PlayerMovement : MonoBehaviour
         timer_jumpLock -= Time.deltaTime;
 
         //jump
-        if (playerInput.actions["Jump"].triggered && timer_coyoteTime > 0f && timer_jumpLock < 0f)
+        if (jumpedThisFrame && timer_coyoteTime > 0f && timer_jumpLock < 0f)
         {
-            jumpedThisFrame = true;
+            queueJump = true;
             timer_coyoteTime = 0f;
             timer_jumpLock = jumpLock;
+            jumpedThisFrame = false;
         }
     }
 
@@ -58,13 +66,13 @@ public class PlayerMovement : MonoBehaviour
         rb.linearVelocityX = movement.x * horizontalSpeed;
 
         //apply jump value
-        if( jumpedThisFrame )
+        if( queueJump )
         {
             Vector2 v = rb.linearVelocity;
             v.y = jumpForce;
             rb.linearVelocity = v;
 
-            jumpedThisFrame = false;
+            queueJump = false;
         }
 
         ////bring player down faster on downward movement
