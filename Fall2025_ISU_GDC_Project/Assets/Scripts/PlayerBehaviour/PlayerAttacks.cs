f20f0a9363072e165004d5ff0fab85bf1188c2db
Brandev100 <bbyers287@aol.com>
2025-09-15 09:33:37 -0500

    Combo glitch fixed
    Stopped a glitch from occurring where attacks would occur out of order.

--- Fall2025_ISU_GDC_Project/Assets/Scripts/PlayerBehaviour/PlayerAttacks.cs
+++ Fall2025_ISU_GDC_Project/Assets/Scripts/PlayerBehaviour/PlayerAttacks.cs
@@ -25,7 +25,6 @@ public class PlayerAttacks : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Current heavy attack is :" + heavyComboIndexer);
         if (comboTimer < Time.time)//If too much time has passed in between attacks...
         {
             lightComboIndexer = 0;
@@ -36,30 +35,36 @@ public class PlayerAttacks : MonoBehaviour
 
     public void OnLightAttack(InputAction.CallbackContext context)
     {
-        Debug.Log("Current light attack is :" + heavyComboIndexer);
-        if (!hitboxRef.GetCurrentlyAttacking())
+        if (context.performed)
         {
-            comboTimer = Time.time + comboWindowDuration;
             if (lightComboIndexer > lightAttacks.Count() - 1)
             {
                 lightComboIndexer = 0;
             }
-            playerAnimator.Play(lightAttacks[lightComboIndexer].name);
-            lightComboIndexer += 1;
+            if (!hitboxRef.GetCurrentlyAttacking())
+            {
+                comboTimer = Time.time + comboWindowDuration;
+                playerAnimator.Play(lightAttacks[lightComboIndexer].name);
+                lightComboIndexer += 1;
+            }
+            
         }
     }
 
     public void OnHeavyAttack(InputAction.CallbackContext context)
     {
-        if (!hitboxRef.GetCurrentlyAttacking())
+        if (context.performed)
         {
-            comboTimer = Time.time + comboWindowDuration;
             if (heavyComboIndexer > heavyAttacks.Count() - 1)
             {
                 heavyComboIndexer = 0;
             }
-            playerAnimator.Play(heavyAttacks[heavyComboIndexer].name);
-            heavyComboIndexer += 1;
+            if (!hitboxRef.GetCurrentlyAttacking())
+            {
+                comboTimer = Time.time + comboWindowDuration;
+                playerAnimator.Play(heavyAttacks[heavyComboIndexer].name);
+                heavyComboIndexer += 1;
+            }
         }
     }
     
