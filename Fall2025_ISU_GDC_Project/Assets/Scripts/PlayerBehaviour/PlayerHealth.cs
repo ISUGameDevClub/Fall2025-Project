b4d23c128b877d5cd11bb9d3d9181f7a1406a1e5
brycejodison <brycejodison2587@gmail.com>
2025-09-18 17:20:10 -0500

    Code that enablels a victory screen to display.
    Description in the README file

--- Fall2025_ISU_GDC_Project/Assets/Scripts/PlayerBehaviour/PlayerHealth.cs
+++ Fall2025_ISU_GDC_Project/Assets/Scripts/PlayerBehaviour/PlayerHealth.cs
@@ -1,12 +1,28 @@
 using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int HP = 100;
+    [SerializeField] private int totalStocks = 3;
+
+    [SerializeField] private Rigidbody2D playerRB;
+    [SerializeField] private bool active = false;
+
+    //Event for death of character (no more stocks)
+    public UnityEvent playerDeath;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        //Get players RB 
+        playerRB = GetComponent<Rigidbody2D>();
+        //Add listeners (We must do this here (and not in heirarchy) as player objects are Prefabs)
+        playerDeath.AddListener(GameObject.FindGameObjectWithTag("GameController").GetComponent<GameSequenceManager>().doVictoryStuff);
     }
 
     // Update is called once per frame
@@ -17,10 +33,33 @@ public class PlayerHealth : MonoBehaviour
             Destroy(gameObject);
             //This is in fixed update to not mess up my for each loop in the hitbox properties script.
         }
+        if (totalStocks <= 0 && !active)
+        {
+            active = true;
+            playerDeath.Invoke();
+        }
     }
 
+    //WORRY ABOUT THIS LATER (DISABLE PLAYER MOVE)----
+
+    /* Disables further player movement upon death
+       To be subscribed to playerDeath event*/
+    // private void disablePlayerMovement()
+    // {
+    // }
+
+    //WORRY ABOUT THIS LATER (DISABLE PLAYER MOVE)---- ^^
+
+    /* Deal damage to player
+    /  @param dmg - dmg to take
+    */
     public void TakeDamage(int dmg)
     {
         HP -= dmg;
+    }
+
+    public int GetStocks()
+    {
+        return totalStocks;
     }
 }
