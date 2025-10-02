6e65a7474496ece0a487ed2bf04e73af14eeab2a
AshFromFire <asimone61606@gmail.com>
2025-10-01 21:50:57 -0500

    Scoreboard Minor Update
    Made minor update to ScoreManager including variables for kill and death score values. Prepared to connect ScoreManager with PlayerHealth

--- Fall2025_ISU_GDC_Project/Assets/Scripts/PlayerBehaviour/PlayerHealth.cs
+++ Fall2025_ISU_GDC_Project/Assets/Scripts/PlayerBehaviour/PlayerHealth.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int HP = 100;
+    private ScoreManagerStartup scoreMangerReference;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        scoreMangerReference = FindAnyObjectByType<ScoreManagerStartup>();
     }
 
     // Update is called once per frame
@@ -14,8 +15,10 @@ public class PlayerHealth : MonoBehaviour
     {
         if (HP <= 0)
         {
+            //ScoreKill(enemykiller, thisplayer) here
             Destroy(gameObject);
             //This is in fixed update to not mess up my for each loop in the hitbox properties script.
+
         }
     }
 
